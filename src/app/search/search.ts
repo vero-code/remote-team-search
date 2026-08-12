2eee9d4746204e9ba1eff45fe84f3963e835ca34
vero-code <79207983+vero-code@users.noreply.github.com>
2026-08-12 20:28:03 +0300

    feat(search): add cancel icon

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -8,8 +8,18 @@ import { Component, output } from '@angular/core';
 })
 export class Search {
   closeSearch = output<void>();
+  query = signal('');
 
   onCloseClick() {
     this.closeSearch.emit();
+  }
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.query.set(input.value);
+  }
+
+  onClear() {
+    this.query.set('');
   }
 }
