c0c72655051cf2fa5bca80c86cef1fa74e73c31c
vero-code <79207983+vero-code@users.noreply.github.com>
2026-08-12 13:49:40 +0300

    feat: import search to header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Search } from '../search/search';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Search],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  isSearchOpen = signal(false);
+
+  openSearch() {
+    this.isSearchOpen.set(true);
+  }
+
+  closeSearch() {
+    this.isSearchOpen.set(false);
+  }
+}
