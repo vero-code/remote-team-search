786a2b5b7d6e5853d27dde8de2febb72cf0631ed
vero-code <79207983+vero-code@users.noreply.github.com>
2026-08-12 14:41:50 +0300

    feat: interaction between header and search

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Search } from './search/search';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Search],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('remote-team-search');
+
+  isSearchOpen = signal(false);
 }
