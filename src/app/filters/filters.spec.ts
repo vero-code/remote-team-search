9386af4c17beef908c9391bb4f695ba87320ccf5
vero-code <79207983+vero-code@users.noreply.github.com>
2026-08-12 20:50:30 +0300

    feat: create filters component

---
+++ src/app/filters/filters.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Filters } from './filters';
+
+describe('Filters', () => {
+  let component: Filters;
+  let fixture: ComponentFixture<Filters>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Filters],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Filters);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/filters/filters.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  imports: [],
+  templateUrl: './filters.html',
+  styleUrl: './filters.scss',
+})
+export class Filters {}
