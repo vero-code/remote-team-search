25fe7bd122808b4a52de46a637fbea78382f7b82
vero-code <79207983+vero-code@users.noreply.github.com>
2026-08-13 20:43:55 +0300

    feat(filters): add items

--- src/app/filters/filters.ts
+++ src/app/filters/filters.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './filters.html',
   styleUrl: './filters.scss',
 })
-export class Filters {}
+export class Filters {
+  options = [
+    { id: 'participant', label: 'Я участник', icon: 'icons/item.svg' },
+    { id: 'inHeaders', label: 'В заголовках', icon: 'icons/item.svg' },
+    { id: 'strictSearch', label: 'Строгий поиск', icon: 'icons/item.svg' },
+  ]
+}
