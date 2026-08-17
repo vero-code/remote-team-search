e2793dda67a98c31874f2d89906bf0026f4fe954
vero-code <79207983+vero-code@users.noreply.github.com>
2026-08-17 15:45:40 +0300

    refactor: show actions btn

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -10,10 +10,8 @@ export class Header {
   openSearch = output<void>();
   isDesktopSearchOpen = signal(false);
 
-  onSearchClick() {
-    if (window.innerWidth >= 768) {
-      this.isDesktopSearchOpen.update(v => !v);
-    } else {
+  onActionClick(id: string) {
+    if (id === 'search') {
       this.openSearch.emit();
     }
   }
@@ -24,5 +22,12 @@ export class Header {
     { id: 'tags', label: 'Теги', icon: 'icons/tags.svg', href: '#' },
     { id: 'favorites', label: 'Избранное', icon: 'icons/favorites.svg', href: '#' },
     { id: 'visits', label: 'Посещения', icon: 'icons/visits.svg', href: '#' },
+  ]
+
+  actions = [
+    { id: 'plus',  label: 'Добавить', icon: 'icons/add.svg', class: 'header__btn header__btn--plus' },
+    { id: 'history',  label: 'История', icon: 'icons/history.svg', class: 'header__btn header__btn--history' },
+    { id: 'search',  label: 'Поиск', icon: 'icons/search.svg', class: 'header__btn header__btn--search' },
+    { id: 'bell', label: 'Уведомления', icon: 'icons/notifications.svg', class: 'header__btn header__btn--bell', badge: 32 },
   ]
 }
